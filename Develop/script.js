a22582f420afa74b8ca5aee26070300b1c48b93f
Neglon <thomasfneylon@gmail.com>
2023-12-07 11:20:58 -0500

    updated user input function and tests
    completed the user input aspect of the generator, with tests to the console to make sure, along with the random array element selector.

--- Develop/script.js
+++ Develop/script.js
@@ -16,9 +16,60 @@ function getInput() {
   console.log(length);
   if (isNaN(length) || length < 8 || length > 126) {
     alert('Must use numbers in the range of 8-126');
-    return null;
+    return;
   }
+  //ask if user wants uppercase in the pw
+  var hasUpperCase = confirm('Do you want the password to contain upper case letters (ABC)? Press OK for yes. Press cancel for no'); 
+  //ask if the user wants lower case in the pw
+  var hasLowerCase = confirm('Do you want the password to contain lower case letters(abc)? Press OK for yes. Press cancel for no');
+  //ask if the user wants numbers in the pw
+  var hasNumbers = confirm('Do you want the password to contain numbers(123)? Press OK for yes. Press cancel for no');
+  //ask if the user wants special characters in the pw
+  var hasSpecialCharacters = confirm('Do you want the password to contain special charcaters (&!$)? Press OK for yes. Press cancel for no');
+  //checking character boolean inputs
+  console.log(hasUpperCase);
+  console.log(hasLowerCase);
+  console.log(hasNumbers);
+  console.log(hasSpecialCharacters);
+  //if user does not select at least one option
+  if (hasUpperCase == false && hasLowerCase == false && hasNumbers == false && hasSpecialCharacters == false) {
+    alert('Password must contain at least one option for character type');
+    return;
+  }
+
+  //object holding the user choice properties
+  var choices = {
+    length: length,
+    hasUppercase: hasUpperCase,
+    hasLowerCase: hasLowerCase,
+    hasNumbers: hasNumbers,
+    hasSpecialCharacters: hasSpecialCharacters
+  }
+  //checking object properties are working and the are holding their boolean values
+  console.log(choices.length);
+  console.log(choices.hasUppercase);
+  console.log(choices.hasLowerCase);
+  console.log(choices.hasNumbers);
+  console.log(choices.hasSpecialCharacters);
+
+  //returns the password choices object to the global scope from this function to be used elsewhere in the program
+  return choices;
 }
+//this function generates a random number to correspond with a spot inside a designated array, it then places that randomly chosen character into a varriable and returns it
+function getRandomIndex(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomCharacter = array[randomIndex];
+
+  return randomCharacter;
+}
+/* for loop to test that the randomizer does infact work as intended.  NOTE i discovered that when returning a variable from a function
+such as randomCharacter, it is not returning the variable name, but only what the variable holds, to use that variable it needs to be redifined outside of the functions scope that 
+it came from */
+for (i=0; i <5; i++) {
+  var randomChar = getRandomIndex(upperCase);
+  console.log(randomChar);
+}
+
 
 
 
